10ac66f5f27ef94545a5055c797bf718a6cb168e
Jason Cameron <dev.jasoncameron@gmail.com>
2023-03-06 19:37:40 -0600

    grab only the text.  this is awesome

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import re
 
 from pdfminer.high_level import extract_pages, extract_text
 
-for page_layout in extract_pages("sample.pdf"):
-    for element in page_layout:
-        print(element)
+# for page_layout in extract_pages("sample.pdf"):
+#     for element in page_layout:
+#         print(element)
+
+write_file = open("text_file.txt", "a")
+
+text = extract_text("sample.pdf")
+write_file.writelines(text)
+print("done")
+
+write_file.close()
