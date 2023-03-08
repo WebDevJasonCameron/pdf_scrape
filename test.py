7e32ae9b5255363a13c319411aee4059f1465e00
Jason Cameron <dev.jasoncameron@gmail.com>
2023-03-07 20:18:20 -0600

    test.py is probably done.  need to change the name to pdf scraper or something

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import argparse
+import glob
+from pdfminer.high_level import extract_pages, extract_text
 
 parser = argparse.ArgumentParser(description='A test program.')
 
@@ -9,13 +11,26 @@ parser.add_argument(
 
 args = parser.parse_args()
 
-print(args.in_dir)
-print(args.out_dir)
-
 flag = True
 
 if args.in_dir == "No Input Directory Arguments provided." or args.out_dir == "No Output Directory Arguments provided.":
     flag = False
 
 if flag:
-    print("The program will not continue")
+    print("Program is running...")
+
+    files = glob.glob(args.in_dir + '/**/*.pdf', recursive=True)
+    files.sort()
+
+    write_file = open(
+        args.out_dir + "/replace_title_with_book_title.md", "a")
+
+    for f in files:
+        text = extract_text(f)
+        write_file.writelines(text)
+
+    write_file.close()
+    print("Program is done.")
+
+else:
+    print("Unable to run Program with out correct arguments.")
