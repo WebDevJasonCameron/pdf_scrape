e5c1b5623b5943c8cf37c1bd7e1f82b6ced91b35
Jason Cameron <dev.jasoncameron@gmail.com>
2023-03-11 11:35:48 -0600

    trying to add double spaces

--- 02_tools/combine_broken_lines.py
+++ 02_tools/combine_broken_lines.py
@@ -3,25 +3,41 @@ import argparse
 parser = argparse.ArgumentParser(description='Removing Parts of a txt doc.')
 
 parser.add_argument(
-    "--focus_file", help="The file you wish to mod.", default="No Arguments provided.")
+    "--file", help="The file you wish to mod.", default="No Arguments provided.")
 
 args = parser.parse_args()
 
 flag = True
+num_count = 0
 
-if args.focus_file == "No Arguments provided.":
+
+def possible_title(line):
+    checker = line.split(" ")
+    if len(checker) <= 3:
+        return True
+    else:
+        return False
+
+
+if args.file == "No Arguments provided.":
     flag = False
 
 if flag:
     print("Program is running...")
 
-    with open(args.focus_file, "r") as f:
+    with open(args.file, "r") as f:
         lines = f.readlines()
 
-    with open(args.focus_file, "w") as f:
+    with open(args.file, "w") as f:
         for line in lines:
-            if not line.endswith("."):
-                line.replace("\\n", " ")
-                f.write(line)
+            if line.startswith("#"):
+                f.write(line + "\n")
+            elif possible_title(line):
+                f.write(line + "\n")
+            elif not line.endswith("."):
+                f.write(line.rstrip())
+                num_count += 1
             else:
-                f.write(line)
+                f.write(line + "\n")
+
+print("Just combined " + str(num_count) + " sentences.")
--- 02_tools/rm_parts.py
+++ 02_tools/rm_parts.py
@@ -3,25 +3,30 @@ import argparse
 parser = argparse.ArgumentParser(description='Removing Parts of a txt doc.')
 
 parser.add_argument(
-    "--focus_file", help="The file you wish to mod.", default="No Arguments provided.")
+    "--file", help="The file you wish to mod.", default="No Arguments provided.")
 parser.add_argument(
-    "--rm_line", help="The line you wish to remove.", default="No Arguements provided")
+    "--rl", help="The line you wish to remove.", default="No Arguements provided")
 
 
 args = parser.parse_args()
 
 flag = True
+num_count = 0
 
-if args.focus_file == "No Arguments provided." or args.rm_line == "No Arguments provided.":
+if args.file == "No Arguments provided." or args.rl == "No Arguments provided.":
     flag = False
 
 if flag:
     print("Program is running...")
 
-    with open(args.focus_file, "r") as f:
+    with open(args.file, "r") as f:
         lines = f.readlines()
 
-    with open(args.focus_file, "w") as f:
+    with open(args.file, "w") as f:
         for line in lines:
-            if line.strip("/n") != args.rm_line:
+            if line.strip("\\n") != args.rl:
                 f.write(line)
+            else:
+                num_count += 1
+
+print("Total lines removed: " + str(num_count))
