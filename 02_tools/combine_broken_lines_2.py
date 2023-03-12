b25e3148c537c57a00b943bcae1aec9780fef510
Jason Cameron <dev.jasoncameron@gmail.com>
2023-03-12 07:31:50 -0500

    still trying to rm double lines

---
+++ 02_tools/combine_broken_lines_2.py
@@ -0,0 +1,42 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='Removing Parts of a txt doc.')
+
+parser.add_argument(
+    "--file", help="The file you wish to mod.", default="No Arguments provided.")
+
+args = parser.parse_args()
+
+flag = True
+num_count = 0
+
+
+def possible_title(line):
+    checker = line.split(" ")
+    if len(checker) <= 3:
+        return True
+    else:
+        return False
+
+
+if args.file == "No Arguments provided.":
+    flag = False
+
+if flag:
+    print("Program is running...")
+
+    with open(args.file, "r") as f:
+        lines = f.readlines()
+
+    with open(args.file, "w") as f:
+        for line in lines:
+            if line.startswith("#"):
+                f.write(line + "\n")
+            elif not line.endswith("."):
+                f.write(line.rstrip())
+            elif possible_title(line):
+                f.write(line + "\n")
+            else:
+                f.write(line + "\n")
+
+print("Just removed " + str(num_count) + " sentences.")
