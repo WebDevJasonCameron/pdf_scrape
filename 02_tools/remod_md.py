4c2b0347dcc1219c73a778afdebb700185fdabfb
Jason Cameron <dev.jasoncameron@gmail.com>
2023-04-05 19:24:40 -0500

    need to add a remodel md tool

---
+++ 02_tools/remod_md.py
@@ -0,0 +1,29 @@
+import argparse
+import glob
+
+parser = argparse.ArgumentParser(
+    description='Remodelling the notion md files to go in a obsidian doc')
+
+parser.add_argument(
+    "--in_f", help="Targeted MD file  used.", default="No Input Directory Arguments provided.")
+# parser.add_argument(
+#     "--out_p", help="The place the new file needs to go.", default="No Output Directory Arguments provided.")
+
+args = parser.parse_args()
+
+flag = True
+line_num = 0
+
+# if args.in_f == "No Input Directory Arguments provided." or args.out_p == "No Output Directory Arguments provided.":
+#     flag = False
+
+if args.in_f == "No Input Directory Arguments provided.":  # remove this in production
+    flag = False
+
+if flag:
+    print("Program is running...")
+
+    target_file = glob.glob(args.in_f)
+
+    for line in target_file:
+        print(line)
