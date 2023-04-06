2764a66c3c4c38ea0d7e741e4c5bd02c681daa60
Jason Cameron <dev.jasoncameron@gmail.com>
2023-04-05 19:38:21 -0500

    rem this all over again

--- 02_tools/remod_md.py
+++ 02_tools/remod_md.py
@@ -6,24 +6,21 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument(
     "--in_f", help="Targeted MD file  used.", default="No Input Directory Arguments provided.")
-# parser.add_argument(
-#     "--out_p", help="The place the new file needs to go.", default="No Output Directory Arguments provided.")
+parser.add_argument(
+    "--out_p", help="The place the new file needs to go.", default="No Output Directory Arguments provided.")
 
 args = parser.parse_args()
 
 flag = True
 line_num = 0
 
-# if args.in_f == "No Input Directory Arguments provided." or args.out_p == "No Output Directory Arguments provided.":
-#     flag = False
-
-if args.in_f == "No Input Directory Arguments provided.":  # remove this in production
+if args.in_f == "No Input Directory Arguments provided." or args.out_p == "No Output Directory Arguments provided.":
     flag = False
 
 if flag:
     print("Program is running...")
 
-    target_file = glob.glob(args.in_f)
+    target_file = open(args.in_f, "r")
 
     for line in target_file:
         print(line)
